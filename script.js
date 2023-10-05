0d8d6997d7d67ee609edc2b2e3302eb7ed64de4e
Abhi <amacharyab22@gmail.com>
2023-10-05 22:46:52 +0530

    update 2nd time

--- script.js
+++ script.js
@@ -5,9 +5,27 @@ menu.onclick = () =>{
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+// let section = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    // section.forEach(sec =>{
+    //     let top = window.scrollY;
+    //     let height = sec.offsetHeight;
+    //     let offset = sec.offsetTop - 150;
+    //     let id = sec.getAttribute('id');
+
+    //     if(top => offset &&  top < offset + height){
+    //         navLinks.forEach(links =>{
+    //             links.classList.remove('active');
+    //             document.querySelectorAll('header .navbar a[href*='+id+']').classList.add('active');
+    //         });
+    //     };
+    // });
 }
 document.querySelector('#search-icon').onclick = () =>{
     document.querySelector('#search-form').classList.toggle('active')
@@ -17,7 +35,7 @@ document.querySelector('#close').onclick = () =>{
 }
 
 var swiper = new Swiper("#forslide", {
-    spaceBetween: 130,
+    spaceBetween: 150,
     centeredSlieds: true,
     autoplay:{
         delay: 2500,
